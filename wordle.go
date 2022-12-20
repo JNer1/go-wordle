27b52a0e6ce949a25ee1d1fb47398e519e5ca0eb
Javier Neri <javierneri81@gmail.com>
2022-12-21 00:32:50 +0800

    fix: convert guess to lowercase

--- wordle.go
+++ wordle.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	answer := "hello"
@@ -8,24 +11,28 @@ func main() {
 	correct_chars := 0
 
 	var text_from_input string
+	var guess string
 
 	for attempts < 3 {
 		fmt.Print("Guess a 5 letter word: ")
 		fmt.Scan(&text_from_input)
 
-		if len(text_from_input) != len(answer) {
+		guess = strings.ToLower(text_from_input)
+		fmt.Print(guess)
+
+		if len(guess) != len(answer) {
 			fmt.Printf("Please choose a word with %v letters\n\n", len(answer))
 			continue
 		}
 
 		for i := 0; i < len(answer); i++ {
 
-			if i >= len(text_from_input) {
+			if i >= len(guess) {
 				fmt.Println("X")
 				continue
 			}
 
-			if text_from_input[i] == answer[i] {
+			if guess[i] == answer[i] {
 				correct_chars++
 				fmt.Print("/")
 			} else {
